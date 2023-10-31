414a1c079da8d58599b147c314f4f3c42932682a
andrew-more <148821905+andrew-more@users.noreply.github.com>
2023-10-31 10:26:38 -0700

    Add BuildTree function to  Main.java

--- src/model/Main.java
+++ src/model/Main.java
@@ -7,4 +7,21 @@ public class Main {
 
 	}
 
+	public static HuffTree buildTree(HuffTree[] trees) {
+   		 MinHeap treeHeap = new MinHeap();
+    		for (HuffTree tree: trees) {
+        		treeHeap.insert(tree);
+    		}
+
+    		HuffTree tmp1, tmp2, tmp3 = null;
+    		while (treeHeap.size() > 1) { // While two items left
+      			tmp1 = treeheap.removeMin();
+      			tmp2 = treeHeap.removeMin();
+      			tmp3 = new HuffTree(tmp1.root(), tmp2.root(),
+                               tmp1.weight() + tmp2.weight());
+     		 	treeHeap.insert(tmp3);   // Return new tree to heap
+   	 	}
+    		return tmp3;            // Return the tree
+  	}
+
 }
